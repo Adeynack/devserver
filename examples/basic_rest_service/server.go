62a676e9c563e11229d125d00ae51410609f0725
Adeynack <Adeynack@users.noreply.github.com>
2019-11-24 23:15:51 +0100

    OAS + Refactor example

--- examples/basic_rest_service/server.go
+++ examples/basic_rest_service/server.go
@@ -14,21 +14,26 @@ import (
 
 func StartServer() {
 	engine := gin.New()
-	engine.Use(gin.Recovery())
-	engine.GET("/persons", getPersonList)
-	engine.GET("/persons/:personID", getPersonById)
-
 	srv := &http.Server{
 		Addr:    "localhost:3000",
 		Handler: engine,
 	}
 
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("ListenAndServe: %s\n", err)
-		}
-	}()
+	engine.Use(gin.Recovery())
 
+	engine.POST("/shutdown", shutdown(srv))
+	engine.GET("/persons", getPersonList)
+	engine.GET("/persons/:personID", getPersonById)
+
+	go shutdownOnInterrupt(srv)
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("ListenAndServe: %s\n", err)
+	}
+	log.Print("Server is now terminated")
+}
+
+func shutdownOnInterrupt(srv *http.Server) {
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt)
 	log.Printf("Shutting down server (received signal %q)", <-quit)
@@ -36,6 +41,21 @@ func StartServer() {
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal("Server Shutdown:", err)
+	}
+}
+
+func shutdown(srv *http.Server) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		go func() {
+			shutdownContext, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			err := srv.Shutdown(shutdownContext)
+			if err != nil {
+				log.Printf("Error shutting down server: %v", err)
+			}
+		}()
+		log.Print("Shutting down server (received POST to /shutdown)")
+		c.String(http.StatusAccepted, "Shutdown request registered.")
 	}
 }
 
